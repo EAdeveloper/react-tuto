4c852109cb8f31231315339500491040bbf379f5
Edward Almanzar <eddeveloper@hotmail.com>
2017-12-10 20:20:13 -0500

    "state" of a component

--- places/src/App.js
+++ places/src/App.js
@@ -8,15 +8,35 @@ import Title from './components/title';
 
 
 class App extends Component {
-  hello(){
-    return "hola bobo";
-  }
+
+  // get the components props
+  constructor(props){
+  //send the same props to parent's construtor, in this case is "Component" from where App extends  
+  super(props);
+
+  //state is a special object to save data and can udate it while the component is executing 
+  this.state ={
+    numero: 0
+  };
+  // use => to preserve the value in 'this'
+  setInterval(()=>{
+    // setState fucntion is to update value for the state
+    // state should be always updated throught the method 'setState'
+    this.setState({
+      numero: this.state.numero + 1
+    })
+  },1000)
+  
+}
+
+
   render() {
     return (
       <section>
         <div>
           <div>
             <Title></Title>
+            <h2>{this.state.numero} </h2>
             <button> Create free account</button>
             <img src={process.env.PUBLIC_URL + "/images/travel.png"} height="300" />
             <div>
